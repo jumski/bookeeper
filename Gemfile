6fa344a1d4e1b32312efb17330b6fb4fad601e4a
Wojtek Majewski <jumski@gmail.com>
2011-11-04 23:35:18 +0100

    added rspec and machinist generators

--- Gemfile
+++ Gemfile
@@ -18,6 +18,10 @@ end
 
 gem 'jquery-rails'
 
+gem 'rspec', :groups => [:test, :development]
+gem 'rspec-rails', :groups => [:test, :development]
+gem 'machinist', :groups => [:test, :development]
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,11 @@ module Bookeeper
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.generators do |g|
+      g.template_engine :haml
+      g.test_framework :rspec, :fixture => true, :views => false
+      g.fixture_replacement => :machinist, :dir => 'spec/blueprints'
+    end
   end
 end
