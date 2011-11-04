c1dce0dd1e63b378a284ec9b1fb4f80e8840bad5
Wojtek Majewski <jumski@gmail.com>
2011-11-04 23:16:22 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bookeeper::Application.config.secret_token = 'ab3d811160f96a4528b98ebaf41676e0a0b911ad040babd1c15e31caa15a6c4fb08dac763d8d575177684537344557e4ce40ef29044d286cb3a64e86d966d826'
